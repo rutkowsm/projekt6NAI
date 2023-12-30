668c8b248a496d0909a3ebf8e3c2d5f5838971fb
marcin <s12497@pjwstk.edu.pl>
2023-12-30 15:48:49 +0100

    rozpoznawanie twarzy

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
-
 import cv2
 
 # Inicjalizacja przechwytywania wideo z pierwszego urządzenia kamery
 cap = cv2.VideoCapture(0)
 
-while True:
-    # Przechwytuje klatka po klatce
-    ret, frame = cap.read()
+# Załaduj klasyfikator Haar do wykrywania twarzy
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-    # Jeśli klatka jest poprawnie przechwycona, ret jest True
+while True:
+    ret, frame = cap.read()
     if not ret:
-        print("Nie można przechwycić obrazu z kamery. Sprawdź podłączenie kamery.")
         break
 
-    # Wyświetlanie klatki
-    cv2.imshow('Obraz z kamery', frame)
+    # Konwersja obrazu do skali szarości
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Wyjdź z pętli, jeśli użytkownik naciśnie 'q'
+    # Wykrywanie twarzy
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    # Rysowanie prostokątów wokół twarzy
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    # Wyświetlanie wyniku
+    cv2.imshow('Rozpoznawanie twarzy', frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Gdy wszystko zostało zakończone, zwolnij uchwyt przechwytywania
 cap.release()
 cv2.destroyAllWindows()
+
